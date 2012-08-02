1ad56236caaf04973a1d165068f712f731e14ca9
Satoshi Ebisawa <e.satoshi@gmail.com>
2012-08-02 20:00:42 +0900

    Initial Commit

---
+++ Sandbox/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Sandbox
+//
+//  Created by Satoshi Ebisawa on 2012/08/02.
+//  Copyright (c) 2012年 Satoshi Ebisawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Sandbox/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Sandbox
+//
+//  Created by Satoshi Ebisawa on 2012/08/02.
+//  Copyright (c) 2012年 Satoshi Ebisawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
